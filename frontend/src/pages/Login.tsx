c10398ea978377e0f8048c04fba1c79ac068cc86
suyan thapa <suyanthapa07@gmail.com>
2025-07-25 12:40:23 +0545

    check

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -52,7 +52,7 @@ const Login :React.FC = () => {
 
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block mb-1 text-sm font-medium">Email</label>
+            <label className="block mb-1 text-sm font-medium">Emailllllllllllllllll</label>
             <input 
                 type="email"
                 className="w-full px-3 py-2 border rounded" 
